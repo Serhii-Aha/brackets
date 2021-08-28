cb4a2b679fa9703d31977b1f04959beeefb9cff4
unknown <a1002android@gmail.com>
2021-08-28 23:13:56 +0300

    как-то так

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 != 0) {
+    return false;
+  } else {
+    return true;
+  }
+
+  let arrayConfigDuble = [];
+  // for (let i = 0; i < bracketsConfig.length; i++) {
+
+  // }
 }
+
+
+// check('()', [['(', ')']]) // -> true
+// check('((()))()', [['(', ')']]) // -> true
+// check('())(', [['(', ')']]) // -> false
+// check('([{}])', [['(', ')'], ['[', ']'], ['{', '}']]) // -> true
+// check('[(])', [['(', ')'], ['[', ']']]) // -> false
+// check('[]()', [['(', ')'], ['[', ']']]) // -> true
+// check('[]()(', [['(', ')'], ['[', ']']]) // -> false
+
+// // special case: opening and closing bracket can be the same :)
+
+// check('||', [['|', '|']]) // -> true
+// check('|()|', [['(', ')'], ['|', '|']]) // -> true
+// check('|(|)', [['(', ')'], ['|', '|']]) // -> false
+// check('|()|(||)||', [['(', ')'], ['|', '|']]) // -> true
