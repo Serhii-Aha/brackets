01222c2ab34adacf1e1780d7d31be5d8782713c6
Ahafonov Serhii <aga31415926@gmail.com>
2022-02-22 13:04:38 +0200

    feat: brackets

--- src/index.js
+++ src/index.js
@@ -9,19 +9,44 @@ module.exports = function check(str, bracketsConfig) {
   arrayConfig = new Set([...array]);
   // console.log(arrayConfig);
   let bracketsConfigFlat = bracketsConfig.flat();
+  let open = [];
+  let close = [];
+  let openIndex, closeIndex;
+  bracketsConfig.forEach(element => {
+    open.push(element[0]);
+    close.push(element[1]);
+  });
   // console.log(bracketsConfigFlat);
-  // arrayConfig.forEach(element => {
-  //   if (!bracketsConfigFlat.includes(element)) {
-  //     return false;
-  //   }       
-  // })
+  arrayConfig.forEach(element => {
+    if (!bracketsConfigFlat.includes(element)) {
+      return false;
+    }       
+  })
+  // return true;
+for (let i = 0, len = array.length; i < len; i++) {
+       openIndex = open.indexOf(array[i]);
+       if (openIndex !== -1) {
+           stack.push(openIndex);
+           continue;
+       }
+       closeIndex = close.indexOf(array[i]);
+       if (closeIndex !== -1) {
+           openIndex = stack.pop();
+           if (closeIndex !== openIndex) {
+               return false;
+           }
+       }
+    }
+    if (stack.length !== 0) {
+        return false;
+    }
+    return true;
+}
 
-
-  return true;
   // for (let i = 0; i < bracketsConfig.length; i++) {
 
   // }
-}
+
 
 
 // check('()', [['(', ')']]) // -> true
