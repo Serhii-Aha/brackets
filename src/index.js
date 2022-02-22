b5653435e8459b6eef4f86a5d6449ac6de618665
Ahafonov Serhii <aga31415926@gmail.com>
2022-02-22 12:31:12 +0200

    feat: includes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
   if (str.length % 2 != 0) {
     return false;
-  } else {
-    return true;
   }
+  // else {
+  //   return true;
+  // }
+  let array = str.split('');
+  arrayConfig = new Set([...array]);
+  // console.log(arrayConfig);
+  let bracketsConfigFlat = bracketsConfig.flat();
+  console.log(bracketsConfigFlat);
+  arrayConfig.forEach(element => {
+    if (!bracketsConfigFlat.includes(element)) {
+      return false;
+    }       
+  })
 
-  let arrayConfigDuble = [];
   // for (let i = 0; i < bracketsConfig.length; i++) {
 
   // }
